2e23378b4ec6578e34c2a011ee2f28f3f343e864
Busolami <oluwabibusola@gmail.com>
2023-05-10 18:32:37 +0100

    outputting list

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,27 @@
-import React from "react";
+//import React from "react";
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    //console.log("hello ninja");
-  };
-  const handleClickAgain = () => {
-    // console.log("hello" + name);
-    setName("Busolami");
-    setAge(21);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  //key attribute should be added to the root the help the react js keep tracks and distinguish the item in the dom
   return (
-    <div className="Home">
-      <h2> HomePage </h2>{" "}
-      <p>
-        {" "}
-        {name} is {age} years old
-      </p>{" "}
-      <button onClick={handleClick}> Click Me </button>{" "}
-      <button onClick={() => handleClickAgain(" Busolami")}>
-        {" "}
-        Click Me Again{" "}
-      </button>{" "}
+    <div className="home">
+      {" "}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2> {blog.title} </h2> <p> Written by {blog.author} </p>{" "}
+        </div>
+      ))}{" "}
     </div>
   );
 };
-
 export default Home;

33e3688b3bc82e8fd0bb8dcc4fc9bee5e1e901ca
Busolami <oluwabibusola@gmail.com>
2023-05-10 17:26:54 +0100

    useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
   const handleClick = () => {
-    console.log("hello ninja");
+    //console.log("hello ninja");
   };
-  const handleClickAgain = (name) => {
-    console.log("hello" + name);
+  const handleClickAgain = () => {
+    // console.log("hello" + name);
+    setName("Busolami");
+    setAge(21);
   };
   return (
     <div className="Home">
-      <h2> HomePage </h2> <button onClick={handleClick}> Click Me </button>{" "}
+      <h2> HomePage </h2>{" "}
+      <p>
+        {" "}
+        {name} is {age} years old
+      </p>{" "}
+      <button onClick={handleClick}> Click Me </button>{" "}
       <button onClick={() => handleClickAgain(" Busolami")}>
         {" "}
         Click Me Again{" "}
